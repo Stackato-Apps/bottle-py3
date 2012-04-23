5c65b6c9a981686c104728420ffce628454a042b
Sridhar Ratnakumar <sridharr@activestate.com>
2012-04-23 16:31:00 -0700

    use PORT

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ def index():
 
 
 if __name__ == '__main__':
-    port = int(os.getenv('VCAP_APP_PORT', '8000'))
-    host = os.getenv('VCAP_APP_HOST', '0.0.0.0')
-    run(host=host, port=port)
+    port = int(os.getenv('PORT', '8000'))
+    run(host='0.0.0.0', port=port)
 
